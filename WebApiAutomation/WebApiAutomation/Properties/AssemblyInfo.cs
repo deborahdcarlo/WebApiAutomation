9864119b068a91957b0b1d71c3660dc750b67ce7
Deborah De Oliveira <deborahdcarlo@gmail.com>
2019-04-15 15:43:22 +0200

    Initial code

---
+++ WebApiAutomation/WebApiAutomation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebApiAutomation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApiAutomation")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0021b454-3a92-4ab1-9a8e-00f21d44f4fa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebApiAutomation/WebApiAutomation/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Serialization.Json;
+
+namespace WebApiAutomation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var client = new RestClient("http://localhost:3000/");
+
+            var request = new RestRequest("auth/login", Method.POST);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new
+            {
+                email = "bruno@email.com",
+                password = "bruno"
+            });
+
+            var response = client.Execute(request);
+
+            JObject obs = JObject.Parse(response.Content);
+            var result = obs["access_token"];
+
+            var request2 = new RestRequest("products/{productid}", Method.GET);
+            request2.AddHeader("Authorization", "Bearer " + result.ToString());
+            request2.AddUrlSegment("productid", 3);
+
+            var response2 = client.Execute(request2);
+
+            //var deserialize = new JsonDeserializer();
+            //var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            //var result = output["name"];
+
+
+            JObject obs2 = JObject.Parse(response2.Content);
+            var result2 = obs2["name"];
+        }
+    }
+}
